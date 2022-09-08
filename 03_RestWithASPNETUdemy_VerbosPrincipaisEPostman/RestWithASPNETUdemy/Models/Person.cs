be2ec6112deb34760690fc881c3f01aeb40bd411
Bruno Gonçalves <Bruno Costa>
2022-09-08 12:03:45 -0300

    Initial commit new project

---
+++ 03_RestWithASPNETUdemy_VerbosPrincipaisEPostman/RestWithASPNETUdemy/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+		public long Id { get; set; };
+		public string FirstName { get; set; };
+		public string LastName { get; set; };
+		public string Address { get; set; };
+		public string Gender { get; set; };
+
+	}
+}
